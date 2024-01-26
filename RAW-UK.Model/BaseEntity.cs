e8936ca1575da33e42a5850e92d1403213f14e9f
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-01-26 13:44:03 +0000

    Created SignUp Model and ViewModel along with Model and UI projects for decoupling

---
+++ RAW-UK.Model/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata;
+
+namespace RAW_UK.Model;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public bool IsArchived { get; set; } = false;
+    public DateTime? ArchivedDate { get; set; }
+}
