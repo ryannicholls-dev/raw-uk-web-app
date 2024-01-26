3e25688dc905b374548be7fc42b1062ec52fe52e
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-01-26 13:27:26 +0000

    further js smoothing

--- RAW-UK/wwwroot/app.js
+++ RAW-UK/wwwroot/app.js
@@ -2,13 +2,15 @@ const hero = document.querySelector('.hero');
 const image = document.querySelector('.bg-img');
 const headline = document.querySelector('.headline');
 const scrollText = document.querySelector('.scroll-text');
+const navBar = document.querySelector('.navbar-nav');
 
 const tl = new TimelineMax();
 
 tl.fromTo(image, 1, { height: "0%"}, {height: "80%", ease: Power2.easeInOut }).delay(0.5)
   .fromTo(image, 1.2, { width: "100%"}, { width: "70%", ease: Power2.easeInOut})
   .fromTo(headline, 0.5, {visibility: "hidden"}, {visibility: "visible"}, "-=1")
-  .fromTo(headline, 1, { opacity: 0 }, { opacity: 1 }, "-=0.5")
+  .fromTo(navBar, 1, { opacity: 0 }, { opacity: 1 }, "-=0.5")
+  .fromTo(headline, 1, { opacity: 0 }, { opacity: 1 }, "-=1")
   .fromTo(scrollText, 1.5, { opacity: 0 }, { opacity: 1 });
 
 //SCROLLMAGIC
