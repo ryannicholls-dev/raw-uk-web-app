c5a2cad740c5accbf9606de59748b029aa66933c
ryannicholls-dev <->
2024-01-27 14:25:48 +0000

    Added snackbar on signup

--- RAW-UK.UI/Components/SignUpForm.razor.cs
+++ RAW-UK.UI/Components/SignUpForm.razor.cs
@@ -1,14 +1,20 @@
-﻿using RAW_UK.Model;
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using RAW_UK.Model;
 
 namespace RAW_UK.UI.Components;
 
 public partial class SignUpForm
 {
-    SignUpViewModel InputViewModel = new SignUpViewModel();
+    [Inject] private ISnackbar snackbar { get; set; }
+    private SignUpViewModel InputViewModel = new SignUpViewModel();
     public void OnHandleValidSubmit()
     {
         SignUp model = InputViewModel.ToModel();
         // await the data service 
         // add snackbar registration succesful message
+        snackbar.Add($"{InputViewModel.FirstName} {InputViewModel.LastName} has signed up for RAW updates");
+        InputViewModel = new();
+        StateHasChanged();
     }
 }
