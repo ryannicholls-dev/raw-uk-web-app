069b0c1c41c7491b3b552552a4e6386ad8f3366a
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-01-27 22:45:11 +0000

    first layer of responsiveness

--- RAW-UK/wwwroot/app.js
+++ RAW-UK/wwwroot/app.js
@@ -1,44 +1,48 @@
 window.blazorInterop = {
-    initializeAnimations: function() {
-        const hero = document.querySelector('.hero');
-        const image = document.querySelector('.bg-img');
-        const headline = document.querySelector('.headline');
-        const scrollText = document.querySelector('.scroll-text');
-        const navBar = document.querySelector('.navbar-nav');
-        const ticker = document.querySelector('.news-ticker-container');
+  initializeAnimations: function() {
+      const isMobileDevice = window.innerWidth <= 768; // Example threshold for mobile devices
+      const imageHeight = isMobileDevice ? "70%" : "80%";
+      const imageWidth = isMobileDevice ? "80%" : "70%";
 
-        const tl = new TimelineMax();
+      const hero = document.querySelector('.hero');
+      const image = document.querySelector('.bg-img');
+      const headline = document.querySelector('.headline');
+      const scrollText = document.querySelector('.scroll-text');
+      const navBar = document.querySelector('.navbar-nav');
+      const ticker = document.querySelector('.news-ticker-container');
 
-        tl.fromTo(image, 1, { height: "0%"}, {height: "80%", ease: Power2.easeInOut }).delay(0.5)
-          .fromTo(image, 1.2, { width: "100%"}, { width: "70%", ease: Power2.easeInOut})
-          .fromTo(headline, 0.5, {visibility: "hidden"}, {visibility: "visible"}, "-=1")
-          .fromTo(navBar, 1, { opacity: 0 }, { opacity: 1 }, "-=0.5")
-          .fromTo(ticker, 3, { opacity: 0 }, { opacity: 1 }, "-=1")
-          .fromTo(headline, 1, { opacity: 0 }, { opacity: 1 }, "-=3")
-          .fromTo(scrollText, 1.5, { opacity: 0 }, { opacity: 1 },"-=2");
+      const tl = new TimelineMax();
 
-        //SCROLLMAGIC
-        const controller = new ScrollMagic.Controller();
+      tl.fromTo(image, 1, { height: "0%"}, {height: imageHeight, ease: Power2.easeInOut }).delay(0.5)
+        .fromTo(image, 1.2, { width: "100%"}, { width: imageWidth, ease: Power2.easeInOut})
+        .fromTo(headline, 0.5, {visibility: "hidden"}, {visibility: "visible"}, "-=1")
+        .fromTo(navBar, 1, { opacity: 0 }, { opacity: 1 }, "-=0.5")
+        .fromTo(ticker, 3, { opacity: 0 }, { opacity: 1 }, "-=1")
+        .fromTo(headline, 1, { opacity: 0 }, { opacity: 1 }, "-=3")
+        .fromTo(scrollText, 1.5, { opacity: 0 }, { opacity: 1 },"-=2");
 
-        const logoAnim = TweenMax.fromTo(image, 1.5, { opacity: 1 }, { opacity: 0 });
-        const textAnim = TweenMax.fromTo(headline, 1.5, { opacity: 1 }, { opacity: 0 });
-  
-        //SCENES
-        let logoScene = new ScrollMagic.Scene({
-          duration: 500, 
-          triggerElement: hero,
-          triggerHook: 0
-        })
-        .setPin(hero)
-        .setTween(logoAnim)
-        .addTo(controller);
+      //SCROLLMAGIC
+      const controller = new ScrollMagic.Controller();
 
-        let textScene = new ScrollMagic.Scene({
-          duration: 500, 
-          triggerElement: hero,
-          triggerHook: 0
-        })
-        .setTween(textAnim)
-        .addTo(controller);
-    }
+      const logoAnim = TweenMax.fromTo(image, 1.5, { opacity: 1 }, { opacity: 0 });
+      const textAnim = TweenMax.fromTo(headline, 1.5, { opacity: 1 }, { opacity: 0 });
+
+      //SCENES
+      let logoScene = new ScrollMagic.Scene({
+        duration: 500, 
+        triggerElement: hero,
+        triggerHook: 0
+      })
+      .setPin(hero)
+      .setTween(logoAnim)
+      .addTo(controller);
+
+      let textScene = new ScrollMagic.Scene({
+        duration: 500, 
+        triggerElement: hero,
+        triggerHook: 0
+      })
+      .setTween(textAnim)
+      .addTo(controller);
+  }
 };
