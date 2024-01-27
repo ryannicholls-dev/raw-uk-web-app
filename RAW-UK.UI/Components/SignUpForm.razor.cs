a4ad1c95102f27bbe325d2e7a3f093149eca90a2
ryannicholls-dev <->
2024-01-27 14:31:03 +0000

    fixed code warning

--- RAW-UK.UI/Components/SignUpForm.razor.cs
+++ RAW-UK.UI/Components/SignUpForm.razor.cs
@@ -6,14 +6,14 @@ namespace RAW_UK.UI.Components;
 
 public partial class SignUpForm
 {
-    [Inject] private ISnackbar snackbar { get; set; }
+    [Inject] private ISnackbar? snackbar { get; set; }
     private SignUpViewModel InputViewModel = new SignUpViewModel();
     public void OnHandleValidSubmit()
     {
         SignUp model = InputViewModel.ToModel();
         // await the data service 
         // add snackbar registration succesful message
-        snackbar.Add($"{InputViewModel.FirstName} {InputViewModel.LastName} has signed up for RAW updates");
+        snackbar?.Add($"{InputViewModel.FirstName} {InputViewModel.LastName} has signed up for RAW updates");
         InputViewModel = new();
         StateHasChanged();
     }
