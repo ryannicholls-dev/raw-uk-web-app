ea7e10ae1e4c745224e5b11e833388116eedaec5
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-01-30 21:35:17 +0000

    created the simple data and signup data services

---
+++ RAW-UK.Repository/DataServices/SignUpDataService.cs
@@ -0,0 +1,10 @@
+﻿using RAW_UK.Model;
+
+namespace RAW_UK.Repository;
+
+public class SignUpDataService : SimpleDataService<SignUp>
+{
+    public SignUpDataService(UnitOfWork unitOfWork, GenericRepository<SignUp> repository) : base(unitOfWork, repository)
+    {
+    }
+}
