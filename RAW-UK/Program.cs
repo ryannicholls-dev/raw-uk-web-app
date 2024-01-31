5a85526f92980e6601742da1f252c14eb65caaea
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-01-31 20:24:17 +0000

    added new connectionstring for initial devtest deploy

--- RAW-UK/Program.cs
+++ RAW-UK/Program.cs
@@ -10,13 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
-builder.Services.AddDbContext<RawUKDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDatabase")));
+// builder.Services.AddDbContext<RawUKDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDatabase")));
+builder.Services.AddDbContext<RawUKDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HostedDatabase")));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<SignUpDataService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<RawUKDbContext>();
+    await context.Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
